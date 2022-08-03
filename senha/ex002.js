460e373c7f9bf27f4fa3c5fe0af8b17738041f3d
Jean <jeanlucorrrea@gmail.com>
2022-08-03 08:51:49 -0300

    new commit

---
+++ senha/ex002.js
@@ -0,0 +1,71 @@
+function verificar() { 
+
+var senha = document.getElementById('senhaum');
+var confirma = document.getElementById('senhadois');
+var res = document.getElementById('res')
+var caracteres = "!" || "@";
+
+if (senha.value == confirma.value) {
+    res.innerHTML = "🎉   Sua senha é válida!   🎉"
+} else {
+    res.innerHTML = "⚠️   Sua senha é inválida!   ⚠️"
+} 
+
+res.style.marginTop = '20px'
+
+
+}
+
+function verificar() { 
+
+    var senha = document.getElementById('senhaum');
+    var confirma = document.getElementById('senhadois');
+    var res = document.getElementById('res')
+    var caracteres = "!" || "@";
+    
+    if (senha.value == confirma.value) {
+        res.innerHTML = "🎉   Sua senha é válida!   🎉"
+    } else {
+        res.innerHTML = "⚠️   Sua senha é inválida!   ⚠️"
+    } 
+    
+    res.style.marginTop = '20px'
+    
+    
+    }
+
+    function verifcar() { 
+
+        var senha = document.getElementById('sehaum');
+        var confirma = document.getElementById('sehadois');
+        var res = document.getElementById('res')
+        var caracteres = "!" || "@";
+        
+        if (senha.value == confirma.value) {
+            res.innerHTML = "🎉   Sua senha é válida!   🎉"
+        } else {
+            res.innerHTML = "⚠️   Sua senha é inválida!   ⚠️"
+        } 
+        
+        res.style.marginTop = '20px'
+        
+        
+        }
+
+        function verifica() { 
+
+            var senha = document.getElementById('sehaum');
+            var confirma = document.getElementById('senhdois');
+            var res = document.getElementById('res')
+            var caracteres = "!" || "@";
+            
+            if (senha.value == confirma.value) {
+                res.innerHTML = "🎉   Sua senha é válida!   🎉"
+            } else {
+                res.innerHTML = "⚠️   Sua senha é inválida!   ⚠️"
+            } 
+            
+            res.style.marginTop = '20px'
+            
+            
+            }
