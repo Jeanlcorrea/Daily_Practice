597a388ad5428b45cb1b8e8167aaba0b8e8487be
Jean Correa <102528406+Jeanlcorrea@users.noreply.github.com>
2022-08-11 19:21:52 -0300

    Update ex002.js

--- senha/ex002.js
+++ senha/ex002.js
@@ -17,22 +17,3 @@ res.style.marginTop = '20px'
 }
 
 
-function verificar() { 
-
-var senha = document.getElementById('senhaum');
-var confirma = document.getElementById('senhadois');
-var res = document.getElementById('res')
-var caracteres = "!" || "@";
-
-if (senha.value == confirma.value) {
-    res.innerHTML = "🎉   Sua senha é válida!   🎉"
-} else {
-    res.innerHTML = "⚠️   Sua senha é inválida!   ⚠️"
-} 
-
-res.style.marginTop = '20px'
-
-
-}
-
-
