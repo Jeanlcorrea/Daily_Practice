d7abd9b8c529bffcdb3d4ea286ffba09aaf302ce
Jean <jeanlucorrrea@gmail.com>
2022-08-29 18:47:11 -0300

    29/08

---
+++ ex003/ex003.js
@@ -0,0 +1,13 @@
+function contar() { 
+    var inicio = document.getElementById('inicio');
+    var fim = document.getElementById('fim');
+    var passo = document.getElementById('passo');
+    var res = document.getElementById('res');
+
+    if (inicio == 0){
+        alert ('Escreva um Número Válido!')
+    }
+
+}
+
+
