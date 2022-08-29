5dec8cfc900f056f8efd53cee84259452cf5c52e
Jean Correa <102528406+Jeanlcorrea@users.noreply.github.com>
2022-08-29 18:50:52 -0300

    test

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,12 +1,44 @@
 
-/*
 var c=1
 
 while (c <= 5) {
     console.log(c)
     c++
 }
-*/
+
+
+for (var c=1; c<=5;c++) {
+    console.log(c)
+}
+
+var c=1
+
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+
+for (var c=1; c<=5;c++) {
+    console.log(c)
+}
+var c=1
+
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+
+for (var c=1; c<=5;c++) {
+    console.log(c)
+}
+var c=1
+
+while (c <= 5) {
+    console.log(c)
+    c++
+}
 
 
 for (var c=1; c<=5;c++) {
