1d5df431015197818ec5009a9e62c5203109fb4b
Jean <jeanlucorrrea@gmail.com>
2022-09-06 19:47:15 -0300

    06/09

--- ex003/ex003.js
+++ ex003/ex003.js
@@ -4,10 +4,34 @@ function contar() {
     var passo = document.getElementById('passo');
     var res = document.getElementById('res');
 
-    if (inicio == 0){
-        alert ('Escreva um Número Válido!')
-    }
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        alert ("[ERRO] Faltam dados!")
+    } else {
+        res.innerHTML = "Contando: "
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
 
+        if (p == 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p=1
+        }
+
+        // contagem crescente
+
+        if(i < f) {
+            for (let c = i; c <= f; c += p){ 
+            res.innerHTML += `${c} \u{1F449}`
+        }
+
+        // contagem regressiva
+
+        }else {
+            for (let c = i; c >= f; c -= p){ 
+            res.innerHTML += `${c} \u{1F449}`
+        } res.innerHTML += `\u{1F3C1}`
+    }
+    }
 }
 
 
