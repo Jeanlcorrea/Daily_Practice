02bfaf0edd2de6b65bcac6db12ce08a89c8cc681
Jean <jeanlucorrrea@gmail.com>
2022-09-08 19:24:11 -0300

    08/09

---
+++ aula15/ambiente.js
@@ -0,0 +1,17 @@
+let valores = [8, 2, 4, 4, 6]
+
+console.log(valores)
+
+for (let pos=0; pos <valores.num.length; pos++){
+    console.log(num[pos])
+}
+
+for (let pos=0; pos <valores.num.length; pos++){
+    console.log(num[pos])
+}
+for (let pos=0; pos <valores.num.length; pos++){
+    console.log(num[pos])
+}
+for (let pos=0; pos <valores.num.length; pos++){
+    console.log(num[pos])
+}
---
+++ ex006/ex006.js
@@ -0,0 +1,17 @@
+    let num = document.getElementById('num').value
+    let tab = document.getElementById('seltab');
+    let res = document.querySelector('div#res');
+    let valores = []
+
+function adicionar() {
+    
+    if (num < 1 || num > 100){
+        alert('Digite um número válido')
+    } else {
+        let item = document.createElement('option')
+        item.text = `Valor ${num} foi adicionado!`
+        item.value = `tab${num}`
+        tab.appendChild(item)
+    }
+}
+
