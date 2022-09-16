ccde977c766f154c30ac89beeafd4f21c3a60da7
Jean Correa <102528406+Jeanlcorrea@users.noreply.github.com>
2022-09-16 14:47:46 -0300

    Update ex006.js

--- ex006/ex006.js
+++ ex006/ex006.js
@@ -30,3 +30,56 @@ function adicionar() {
 }
 
 {
+
+    function adicionar() {
+    
+    if (num < 1 || num > 100){
+        alert('Digite um número válido')
+    } else {
+        let item = document.createElement('option')
+        item.text = `Valor ${num} foi adicionado!`
+        item.value = `tab${num}`
+        tab.appendChild(item)
+    }
+}
+
+{
+    function adicionar() {
+    
+    if (num < 1 || num > 100){
+        alert('Digite um número válido')
+    } else {
+        let item = document.createElement('option')
+        item.text = `Valor ${num} foi adicionado!`
+        item.value = `tab${num}`
+        tab.appendChild(item)
+    }
+}
+
+{
+    function adicionar() {
+    
+    if (num < 1 || num > 100){
+        alert('Digite um número válido')
+    } else {
+        let item = document.createElement('option')
+        item.text = `Valor ${num} foi adicionado!`
+        item.value = `tab${num}`
+        tab.appendChild(item)
+    }
+}
+
+{
+    function adicionar() {
+    
+    if (num < 1 || num > 100){
+        alert('Digite um número válido')
+    } else {
+        let item = document.createElement('option')
+        item.text = `Valor ${num} foi adicionado!`
+        item.value = `tab${num}`
+        tab.appendChild(item)
+    }
+}
+
+{
